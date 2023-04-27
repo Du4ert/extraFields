0c006daaeea5e5f568d0b14e09c17b9fa06ef2ff
Du4ert <du4ertb@gmail.com>
2023-04-27 12:27:52 +0300

    add custom guidelines field

--- ExtraFieldsPlugin.inc.php
+++ ExtraFieldsPlugin.inc.php
@@ -23,11 +23,15 @@ class ExtraFieldsPlugin extends GenericPlugin
     {
         $success = parent::register($category, $path);
         if ($success && $this->getEnabled()) {
-        // Use a hook to extend the context entity's schema
-        HookRegistry::register('Schema::get::publication', array($this, 'addToSchema'));
+            // Use a hook to extend the context entity's schema
+            HookRegistry::register('Schema::get::publication', array($this, 'addToSchemaPublication'));
 
-        // Use a hook to add a field to the title-abstract publicaion settings.
-        HookRegistry::register('Form::config::before', array($this, 'addToForm'));
+            HookRegistry::register('Schema::get::context', array($this, 'addToSchemaContext'));
+
+            // Use a hook to add a field to the title-abstract publicaion settings.
+            HookRegistry::register('Form::config::before', array($this, 'addToFormPublication'));
+
+            HookRegistry::register('TemplateManager::display', array($this, 'addCustomVars'));
         }
         return $success;
     }
@@ -59,47 +63,112 @@ class ExtraFieldsPlugin extends GenericPlugin
     }
 
 
-	/**
-	 * Add a property to the context schema
-	 *
-	 * @param $hookName string `Schema::get::context`
-	 * @param $args [[
-	 * 	@option object Context schema
-	 * ]]
-	 */
-	public function addToSchema($hookName, $args) {
-		$schema = $args[0];
-        $schema->properties->funding = ( object ) [
-			'type' => 'string',
-			'apiSummary' => true,
-			'multilingual' => true,
-			'validation' => ['nullable']
+    /**
+     * Add a property to the context schema
+     *
+     * @param $hookName string `Schema::get::context`
+     * @param $args [[
+     * 	@option object Context schema
+     * ]]
+     */
+    public function addToSchemaPublication($hookName, $args)
+    {
+        $schema = $args[0];
+        $schema->properties->funding = (object) [
+            'type' => 'string',
+            'apiSummary' => true,
+            'multilingual' => true,
+            'validation' => ['nullable']
         ];
-	}
+    }
 
-	/**
-	 * Add a form field to a form
-	 *
-	 * @param $hookName string `Form::config::before`
-	 * @param $form FormHandler
-	 */
-	public function addtoForm($hookName, $form) {
-		if (!defined('FORM_METADATA') || $form->id !== FORM_METADATA) {
-			return;
-		}
+    public function addToSchemaContext($hookName, $args)
+    {
+        $schema = $args[0];
+        $schema->properties->customGuidelines = (object) [
+            'type' => 'string',
+            'apiSummary' => true,
+            'multilingual' => true,
+            'validation' => ['nullable']
+        ];
+    }
 
-		$context = Application::get()->getRequest()->getContext();
+    /**
+     * Add a form field to a form
+     *
+     * @param $hookName string `Form::config::before`
+     * @param $form FormHandler
+     */
+    public function addtoFormPublication($hookName, $form)
+    {
+        if (defined('FORM_METADATA') && $form->id === FORM_METADATA) {
+            $this->addFundingField($form);
+            return;
+        }
 
-		if (!$context) {
-			return;
-		}
+        if (defined('FORM_AUTHOR_GUIDELINES') && $form->id === FORM_AUTHOR_GUIDELINES) {
+            $this->addCustomAuthorGuidelinesField($form);
+            return;
+        }
+    }
 
-		$form->addField(new \PKP\components\forms\FieldTextarea('funding', [
-			'label' => 'Funding',
-			// 'groupId' => 'publishing',
+
+    private function addFundingField($form)
+    {
+        $context = Application::get()->getRequest()->getContext();
+
+        if (!$context) {
+            return;
+        }
+
+        $form->addField(new \PKP\components\forms\FieldTextarea('funding', [
+            'label' => __('plugins.generic.extraFields.funding'),
+            // 'groupId' => 'publishing',
             'size' => 'medium',
             'isMultilingual' => true,
-			'value' => $context->getData('funding'),
-		]));
-	}
+            'value' => $context->getData('funding'),
+        ]));
+    }
+
+
+    private function addCustomAuthorGuidelinesField($form)
+    {
+        $context = Application::get()->getRequest()->getContext();
+
+        if (!$context) {
+            return;
+        }
+
+        $form->addField(new \PKP\components\forms\FieldRichTextarea('customGuidelines', [
+            'label' => __('plugins.generic.extraFields.authorGuidelinesCustom'),
+            // 'groupId' => 'publishing',
+            'size' => 'medium',
+            'isMultilingual' => true,
+            'value' => $context->getData('customGuidelines'),
+        ]));
+    }
+
+    /**
+     * Add a var to a template
+     *
+     * @param $hookName string `TemplateManager::display`
+     * @param $templateMgr TemplateManager
+     */
+    public function addCustomVars($hookName, $args)
+    {
+        // Retrieve the TemplateManager
+        $templateMgr = $args[0];
+        $template = $args[1];
+
+        // Don't do anything if we're not loading the right template
+        if ($template != 'frontend/pages/submissions.tpl') {
+            return;
+        }
+
+        $context = Application::get()->getRequest()->getContext();
+
+        $templateMgr->assign([
+            'customGuidelines' => $context->getLocalizedData('customGuidelines'),
+        ]);
+    }
 }
